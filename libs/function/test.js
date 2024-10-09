67e0ee5803dcaabb1573d6e7e0a8cbc4499bd495
仰望星空 <66815206+sunkangchina@users.noreply.github.com>
2024-10-09 21:13:33 +0800

    修正test video

--- libs/function/test.js
+++ libs/function/test.js
@@ -149,23 +149,23 @@ function landline(value) {
  */
 function empty(value) {
     switch (typeof value) {
-    case 'undefined':
-        return true
-    case 'string':
-        if (value.replace(/(^[ \t\n\r]*)|([ \t\n\r]*$)/g, '').length == 0) return true
-        break
-    case 'boolean':
-        if (!value) return true
-        break
-    case 'number':
-        if (value === 0 || isNaN(value)) return true
-        break
-    case 'object':
-        if (value === null || value.length === 0) return true
-        for (const i in value) {
-            return false
-        }
-        return true
+        case 'undefined':
+            return true
+        case 'string':
+            if (value.replace(/(^[ \t\n\r]*)|([ \t\n\r]*$)/g, '').length == 0) return true
+            break
+        case 'boolean':
+            if (!value) return true
+            break
+        case 'number':
+            if (value === 0 || isNaN(value)) return true
+            break
+        case 'object':
+            if (value === null || value.length === 0) return true
+            for (const i in value) {
+                return false
+            }
+            return true
     }
     return false
 }
@@ -242,7 +242,7 @@ function image(value) {
  * @param {Object} value
  */
 function video(value) {
-    const VIDEO_REGEXP = /\.(mp4|mpg|mpeg|dat|asf|avi|rm|rmvb|mov|wmv|flv|mkv|m3u8)$/i
+    const VIDEO_REGEXP = /\.(mp4|mpg|mpeg|dat|asf|rm|rmvb|mov|wmv|flv|mkv|m3u8)$/i
     return VIDEO_REGEXP.test(value)
 }
 
